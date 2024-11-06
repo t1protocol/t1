396a18e4ad1f2d28c15ee4892e7e39e848a37e0d
Krzysztof Spisak-Spisacki <krzysztof@t1protocol.com>
2024-11-06 14:55:49 -0500

    Trigger l1 update on any new tx (#1)
    * trigger L1 update on any transaction rather than
    
    * chore: remove redundant logging
    
    * removed redundant log
    
    ---------
    
    Co-authored-by: Krzysztof Spisak-Spisacki <mafiagustawa@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
 #![allow(missing_docs)]
 
 use std::future::Future;
-use reth_node_ethereum::EthereumNode;
-
-use alloy_sol_types::{sol, SolEventInterface};
-use alloy_primitives::Address;
 use futures::StreamExt;
 use tracing::info;
-use reth_execution_types::Chain;
-use reth_primitives::{SealedBlockWithSenders, TransactionSigned};
 
+use reth_execution_types::Chain;
+use reth_primitives::TransactionSigned;
 use reth_exex::{ExExContext, ExExEvent, ExExNotification};
 use reth_node_api::FullNodeComponents;
+use reth_node_ethereum::EthereumNode;
 
-sol!(CounterContract, "abi/counter_abi.json");
-use CounterContract::{CounterContractEvents};
 use secp256k1::SecretKey;
 use std::str::FromStr;
 use web3::contract::{Contract, Options};
@@ -62,100 +57,68 @@ async fn exex_init<Node: FullNodeComponents>(
 
 /// An ExEx is just a future, which means you can implement all of it in an async function!
 ///
-/// This ExEx just prints out whenever either a new chain of blocks being added, or a chain of
-/// blocks being re-orged. After processing the chain, emits an [ExExEvent::FinishedHeight] event.
+/// Logs new chain events and notifies L1 on chain commit
 async fn exex<Node: FullNodeComponents>(mut ctx: ExExContext<Node>) -> eyre::Result<()> {
     while let Some(notification) = ctx.notifications.next().await {
         match &notification? {
             ExExNotification::ChainCommitted { new } => {
-                info!(committed_chain = ?new.range(), "Received commit");
-                info!("Current stateRoot is [{}]", new.tip().block.header.state_root);
                 notify_l1(&new).await;
 
                 ctx.events.send(ExExEvent::FinishedHeight(new.tip().num_hash()))?;
             }
-            ExExNotification::ChainReorged { old, new } => {
-                info!(from_chain = ?old.range(), to_chain = ?new.range(), "Received reorg");
-            }
-            ExExNotification::ChainReverted { old } => {
-                info!(reverted_chain = ?old.range(), "Received revert");
-            }
+            _ => {}
         };
     }
 
     Ok(())
 }
 
-/// Decode chain of blocks into a flattened list of receipt logs, filter only transactions to the
-/// Counter contract [COUNTER_CONTRACT_ADDRESS] and extract [CounterContractEvents].
-fn decode_chain_into_rollup_events(
+/// Decode chain of blocks into a flattened list of new transactions
+fn decode_chain_into_new_transactions(
     chain: &Chain,
-) -> Vec<(&SealedBlockWithSenders, &TransactionSigned, CounterContractEvents)> {
-    let counter_contract_address = std::env::var("COUNTER_CONTRACT_ADDRESS")
-        .expect("COUNTER_CONTRACT_ADDRESS environment variable not set");
-    let counter_contract_address = Address::from_str(&counter_contract_address).unwrap();
+) -> Vec<&TransactionSigned> {
     chain
         // Get all blocks and receipts
         .blocks_and_receipts()
         // Get all receipts
-        .flat_map(|(block, receipts)| {
-            block
+        .flat_map(|(block, _)| {
+            &block
                 .body
                 .transactions
-                .iter()
-                .zip(receipts.iter().flatten())
-                .map(move |(tx, receipt)| (block, tx, receipt))
-        })
-        // Get all logs from counter contract
-        .flat_map(|(block, tx, receipt)| {
-            receipt
-                .logs
-                .iter()
-                .filter(|log| log.address == counter_contract_address)
-                .map(move |log| (block, tx, log))
-        })
-        // Decode and filter counter events
-        .filter_map(|(block, tx, log)| {
-            CounterContractEvents::decode_raw_log(log.topics(), &log.data.data, true)
-                .ok()
-                .map(|event| (block, tx, event))
         })
         .collect()
 }
 
-
+/// Sends a notification to the State Root contract if there were new transactions
 async fn notify_l1(chain: &Chain) {
-    let events = decode_chain_into_rollup_events(chain);
+    let new_transactions = decode_chain_into_new_transactions(chain);
 
-    for (_, _tx, event) in events {
-        match event {
-            CounterContractEvents::Incremented(..) => {
-                let transport = std::env::var("L1_RPC_ADDRESS")
-                    .expect("L1_RPC_ADDRESS environment variable not set");
-                let transport = Http::new(&transport).unwrap();
-                let web3 = web3::Web3::new(transport);
-                let state_root_contract = std::env::var("STATE_ROOT_CONTRACT_ADDRESS")
-                    .expect("STATE_ROOT_CONTRACT_ADDRESS environment variable not set");
-                let state_root_contract = StateRootContract::new(
-                    &web3,
-                    &state_root_contract,
-                ).await;
-                let wallet = std::env::var("PREFUNDED_SECRET")
-                    .expect("PREFUNDED_SECRET environment variable not set");
-                let wallet = SecretKey::from_str(&wallet).unwrap();
+    if !new_transactions.is_empty() {
+        let transport = std::env::var("L1_RPC_ADDRESS")
+            .expect("L1_RPC_ADDRESS environment variable not set");
+        let transport = Http::new(&transport).unwrap();
+        let web3 = web3::Web3::new(transport);
+        let state_root_contract = std::env::var("STATE_ROOT_CONTRACT_ADDRESS")
+            .expect("STATE_ROOT_CONTRACT_ADDRESS environment variable not set");
+        let state_root_contract = StateRootContract::new(
+            &web3,
+            &state_root_contract,
+        ).await;
+        let wallet = std::env::var("PREFUNDED_SECRET")
+            .expect("PREFUNDED_SECRET environment variable not set");
+        let wallet = SecretKey::from_str(&wallet).unwrap();
 
-                let tx_id = state_root_contract
-                    .update_state_root(
-                        &wallet, Bytes::from(chain.tip().block.header.state_root.to_vec())
-                    );
+        let tx_id = state_root_contract
+            .update_state_root(
+                &wallet, Bytes::from(chain.tip().block.header.state_root.to_vec())
+            );
 
-                match tx_id.await {
-                    Ok(id) => info!("I notifed L1 with new state root. txId = [{:#x}]", id),
-                    Err(error) => info!("I failed to notify L1 with new state root. error = [{}]", error)
-                }
-
-                ()
-            }
+        match tx_id.await {
+            Ok(id) => info!(
+                "I found {} new transactions and notifed L1 with new state root. txId = [{:#x}]",
+                new_transactions.len(), id
+            ),
+            Err(error) => info!("I failed to notify L1 with new state root. error = [{}]", error)
         }
     }
 }
