79ab7c243b3d185120131e519927cb413356ebd8
Krzysztof Spisak-Spisacki <mafiagustawa@gmail.com>
2024-12-30 06:13:00 +0100

    feat: messenger mock interaction script

--- contracts/script/TriggerT1L1MessengerMock.s.sol
+++ contracts/script/TriggerT1L1MessengerMock.s.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.28;
 import { Script } from "forge-std/Script.sol";
 
 import {T1L1MessengerMock} from "../src/mocks/T1L1MessengerMock.sol";
+import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 // solhint-disable max-states-count
 // solhint-disable state-visibility
@@ -13,15 +14,15 @@ import {T1L1MessengerMock} from "../src/mocks/T1L1MessengerMock.sol";
 contract InitializeL1BridgeContracts is Script {
     uint256 L1_DEPLOYER_PRIVATE_KEY = vm.envUint("L1_DEPLOYER_PRIVATE_KEY");
 
-    address T1L1_MESSENGER_MOCK_ADDR = address("0x373E0B8B80A15cdf587C1263654c6B5edd195a43");
+    address T1L1_MESSENGER_MOCK_ADDR = makeAddr("0x373E0B8B80A15cdf587C1263654c6B5edd195a43");
 
     function run() external {
 
         vm.startBroadcast(L1_DEPLOYER_PRIVATE_KEY);
 
-        address to = address("0xdeadbeef");
+        address to = makeAddr("0xdeadbeef");
         uint256 value = 1000000000;
-        bytes memory message = 0x0;
+        bytes memory message = ProxyAdmin.changeProxyAdmin.selector;
         uint256 gasLimit = 10000000000000;
 
         T1L1MessengerMock(T1L1_MESSENGER_MOCK_ADDR).sendMessage(to, value, message, gasLimit);
