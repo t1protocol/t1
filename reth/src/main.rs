6e105d5cc1fa257f2af618b1b6482506fb81ffc9
Diego Garcia <diego.gm@pm.me>
2025-01-16 15:41:48 +0100

    Add test to minimal exex

--- reth/src/main.rs
+++ reth/src/main.rs
@@ -1,12 +1,24 @@
-use futures_util::TryStreamExt;
-use reth::{api::FullNodeComponents, builder::NodeTypes, primitives::EthPrimitives};
+use futures::{Future, TryStreamExt};
 use reth_exex::{ExExContext, ExExEvent, ExExNotification};
+use reth_node_api::FullNodeComponents;
 use reth_node_ethereum::EthereumNode;
 use reth_tracing::tracing::info;
 
-async fn t1_exex<Node: FullNodeComponents<Types: NodeTypes<Primitives = EthPrimitives>>>(
-    mut ctx: ExExContext<Node>,
-) -> eyre::Result<()> {
+/// The initialization logic of the ExEx is just an async function.
+///
+/// During initialization you can wait for resources you need to be up for the ExEx to function,
+/// like a database connection.
+async fn exex_init<Node: FullNodeComponents>(
+    ctx: ExExContext<Node>,
+) -> eyre::Result<impl Future<Output = eyre::Result<()>>> {
+    Ok(exex(ctx))
+}
+
+/// An ExEx is just a future, which means you can implement all of it in an async function!
+///
+/// This ExEx just prints out whenever either a new chain of blocks being added, or a chain of
+/// blocks being re-orged. After processing the chain, emits an [ExExEvent::FinishedHeight] event.
+async fn exex<Node: FullNodeComponents>(mut ctx: ExExContext<Node>) -> eyre::Result<()> {
     while let Some(notification) = ctx.notifications.try_next().await? {
         match &notification {
             ExExNotification::ChainCommitted { new } => {
@@ -32,10 +44,50 @@ fn main() -> eyre::Result<()> {
     reth::cli::Cli::parse_args().run(|builder, _| async move {
         let handle = builder
             .node(EthereumNode::default())
-            .install_exex("t1",  |ctx| async move { Ok(t1_exex(ctx)) })
+            .install_exex("Minimal", exex_init)
             .launch()
             .await?;
 
         handle.wait_for_node_exit().await
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use reth_execution_types::{Chain, ExecutionOutcome};
+    use reth_exex_test_utils::{test_exex_context, PollOnce};
+    use std::pin::pin;
+
+    #[tokio::test]
+    async fn test_exex() -> eyre::Result<()> {
+        // Initialize a test Execution Extension context with all dependencies
+        let (ctx, mut handle) = test_exex_context().await?;
+
+        // Save the current head of the chain to check the finished height against it later
+        let head = ctx.head;
+
+        // Send a notification to the Execution Extension that the chain has been committed
+        handle
+            .send_notification_chain_committed(Chain::from_block(
+                handle.genesis.clone(),
+                ExecutionOutcome::default(),
+                None,
+            ))
+            .await?;
+
+        // Initialize the Execution Extension
+        let mut exex = pin!(super::exex_init(ctx).await?);
+
+        // Check that the Execution Extension did not emit any events until we polled it
+        handle.assert_events_empty();
+
+        // Poll the Execution Extension once to process incoming notifications
+        exex.poll_once().await?;
+
+        // Check that the Execution Extension emitted a `FinishedHeight` event with the correct
+        // height
+        handle.assert_event_finished_height((head.number, head.hash).into())?;
+
+        Ok(())
+    }
 }
