d76debfe4512f1ba69c9dfaec3247a67eba00302
evchip <evchip@pm.me>
2025-01-27 11:44:05 +0100

    refactor(T1Constants): change L1_CHAIN_ID to Sepolia

--- contracts/src/libraries/constants/T1Constants.sol
+++ contracts/src/libraries/constants/T1Constants.sol
@@ -10,8 +10,8 @@ library T1Constants {
     /// @dev The first 20 bytes of keccak("drop")
     address internal constant DROP_XDOMAIN_MESSAGE_SENDER = 0x6f297C61B5C92eF107fFD30CD56AFFE5A273e841;
 
-    /// @notice Chain ID of the Ethereum network
-    uint64 internal constant ETH_CHAIN_ID = 1;
+    /// @notice Chain ID of the L1 network
+    uint64 internal constant L1_CHAIN_ID = 11_155_111;
 
     /// @notice Chain ID of the T1 devnet
     uint64 internal constant T1_DEVNET_CHAIN_ID = 3_151_908;
