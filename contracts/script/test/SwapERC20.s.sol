10adcd1fb5ccc466be0a6b7eeda17e684485e092
Wojciech Wołoszyn <w-woloszyn@users.noreply.github.com>
2025-02-20 23:33:49 +0100

    feat: automation, fixes, and genesis updates (#46)
    Co-authored-by: Diego Garcia <diego.gm@pm.me>

--- contracts/script/test/SwapERC20.s.sol
+++ contracts/script/test/SwapERC20.s.sol
@@ -16,6 +16,7 @@ import { PermitSignature } from "../../src/test/utils/PermitSignature.sol";
 import { T1Constants } from "../../src/libraries/constants/T1Constants.sol";
 
 // solhint-disable var-name-mixedcase
+// solhint-disable reason-string
 
 contract SwapERC20 is Script, PermitSignature {
     address private L1_GATEWAY_ROUTER_PROXY_ADDR = vm.envAddress("L1_GATEWAY_ROUTER_PROXY_ADDR");
@@ -24,12 +25,14 @@ contract SwapERC20 is Script, PermitSignature {
     address private L1_USDT_ADDR = vm.envAddress("L1_USDT_ADDR");
     uint256 private ALICE_PRIVATE_KEY = vm.envUint("ALICE_PRIVATE_KEY");
     address private alice = vm.addr(ALICE_PRIVATE_KEY);
-    uint256 private inputTokenAmount = 1e15; // WETH
-    uint256 private outputTokenAmount = 10e18; // USDT
+    uint256 private MARKET_MAKER_PRIVATE_KEY = vm.envUint("MARKET_MAKER_PRIVATE_KEY");
+    address private marketMaker = vm.addr(MARKET_MAKER_PRIVATE_KEY);
+    uint256 private inputTokenAmount = 0.0001 ether; // WETH
+    uint256 private outputTokenAmount = 1 ether; // USDT
 
     function run() external {
         vm.createSelectFork(vm.rpcUrl("sepolia"));
-        vm.startBroadcast(ALICE_PRIVATE_KEY);
+        vm.startBroadcast(MARKET_MAKER_PRIVATE_KEY);
 
         // Alice needs WETH to swap for USDT
         // Bridge should have USDT to swap for WETH
@@ -38,8 +41,8 @@ contract SwapERC20 is Script, PermitSignature {
 
         ISignatureTransfer.PermitTransferFrom memory permit = ISignatureTransfer.PermitTransferFrom({
             permitted: ISignatureTransfer.TokenPermissions({ token: L1_WETH_ADDR, amount: inputTokenAmount }),
-            nonce: 0,
-            deadline: block.timestamp + 1000
+            nonce: uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.prevrandao))),
+            deadline: block.timestamp + 10_000_000
         });
 
         IL1GatewayRouter.Witness memory witness = IL1GatewayRouter.Witness({
@@ -71,11 +74,11 @@ contract SwapERC20 is Script, PermitSignature {
         }
 
         // Check if the ERC20 gateway has approved the permit2 to transfer USDT
-        // TODO include check allowance transfer to the router on USDT
+        // TODO - Check if allowance has expired
         if (T1StandardERC20(L1_USDT_ADDR).allowance(L1_STANDARD_ERC20_GATEWAY_PROXY_ADDR, permit2) < outputTokenAmount)
         {
             IL1StandardERC20Gateway(L1_STANDARD_ERC20_GATEWAY_PROXY_ADDR).allowRouterToTransfer(
-                L1_USDT_ADDR, type(uint160).max, uint48(block.timestamp + 1000)
+                L1_USDT_ADDR, type(uint160).max, uint48(block.timestamp + 10_000_000)
             );
         }
 
@@ -85,9 +88,9 @@ contract SwapERC20 is Script, PermitSignature {
             "ERC20 gateway doesn't have enough USDT"
         );
 
-        // Use SetMM script to set this address as the router's market maker
+        // Use SetMM script to set alice as the router's market maker
         require(
-            IL1GatewayRouter(L1_GATEWAY_ROUTER_PROXY_ADDR).marketMaker() == address(this),
+            IL1GatewayRouter(L1_GATEWAY_ROUTER_PROXY_ADDR).marketMaker() == marketMaker,
             "Signer is not the market maker in the router"
         );
 
