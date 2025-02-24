4c07fa6fec92ebfc5bcb9999596aaff530e697c7
Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki+1@gmail.com>
2025-02-24 04:23:13 +0100

    chore: moved hardcoded config to .env
    Signed-off-by: Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki+1@gmail.com>

--- e2e/src/config/tests-config/environments/canary.ts
+++ e2e/src/config/tests-config/environments/canary.ts
@@ -6,10 +6,10 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const L1_RPC_URL = new URL(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
-const L2_RPC_URL = new URL("https://rpc.devnet.t1protocol.com");
-const L1_CHAIN_ID = 11155111;
-const L2_CHAIN_ID = 3151908;
+const L1_RPC_URL = new URL(process.env.L1_RPC_URL);
+const L2_RPC_URL = new URL(process.env.L2_RPC_URL);
+const L1_CHAIN_ID: number = +process.env.L1_CHAIN_ID;
+const L2_CHAIN_ID: number = +process.env.L2_CHAIN_ID;
 
 const L1_WHALE_ACCOUNTS_PRIVATE_KEYS: string[] = process.env.L1_WHALE_ACCOUNTS_PRIVATE_KEYS?.split(",") ?? [];
 const L2_WHALE_ACCOUNTS_PRIVATE_KEYS: string[] = process.env.L2_WHALE_ACCOUNTS_PRIVATE_KEYS?.split(",") ?? [];
@@ -28,9 +28,9 @@ const config: Config = {
   L1: {
     rpcUrl: L1_RPC_URL,
     chainId: L1_CHAIN_ID,
-    l1t1messengerContractAddress: "0x30622442E5421C49A8F89e871Bf37D55f8755B0E",
-    standardErc20gatewayContractAddress: "0x840e8C42dF441df6431343F6FCc18869fDC9C917",
-    l1usdtAddress: "0x30E9b6B0d161cBd5Ff8cf904Ff4FA43Ce66AC346",
+    l1t1messengerContractAddress: process.env.L1_T1_MESSENGER_ADDRESS,
+    standardErc20gatewayContractAddress: process.env.L1_STANDARD_ERC20_GATEWAY_ADDRESS,
+    l1usdtAddress: process.env.L1_USDT_ADDRESS,
     accountManager: new EnvironmentBasedAccountManager(
       new ethers.JsonRpcProvider(L1_RPC_URL.toString()),
       L1_WHALE_ACCOUNTS,
@@ -40,9 +40,9 @@ const config: Config = {
   L2: {
     rpcUrl: L2_RPC_URL,
     chainId: L2_CHAIN_ID,
-    l2t1messengerContractAddress: "0x805DA25653A36d60b3739006906Ad82557a0A044",
-    standardErc20gatewayContractAddress: "0xFA80E171524dB33BABC686e73501f1125190264e",
-    l2usdtAddress: "0x337bE36E710f7af68E1fD3DDd48070Cecc5Bb136",
+    l2t1messengerContractAddress: process.env.L2_T1_MESSENGER_ADDRESS,
+    standardErc20gatewayContractAddress: process.env.L2_STANDARD_ERC20_GATEWAY_ADDRESS,
+    l2usdtAddress: process.env.L2_USDT_ADDRESS,
     accountManager: new EnvironmentBasedAccountManager(
       new ethers.JsonRpcProvider(L2_RPC_URL.toString()),
       L2_WHALE_ACCOUNTS,
