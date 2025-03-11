56ff326e1789fa73ed50793bf9d7f46fe33d6642
evchip <evchip@pm.me>
2025-03-11 22:30:51 +0000

    feat(DeployXChainRead): write script to deploy XChainRead
    to t1 and PR1

---
+++ contracts/script/deploy/DeployXChainRead.s.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.25;
+
+import { Script } from "forge-std/Script.sol";
+import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import { DeploymentUtils } from "../lib/DeploymentUtils.sol";
+
+import { T1XChainRead } from "../../src/libraries/x-chain/T1XChainRead.sol";
+import { T1Constants } from "../../src/libraries/constants/T1Constants.sol";
+
+contract DeployXChainRead is Script, DeploymentUtils {
+    uint32 internal origin = uint32(T1Constants.T1_DEVNET_CHAIN_ID);
+    uint32 internal pr1 = 1337; // Arbitrum Private Network
+
+    function deploy_to_t1() external {
+        logStart("DeployXChainRead to t1");
+
+        uint256 L2_DEPLOYER_PRIVATE_KEY = vm.envUint("L2_DEPLOYER_PRIVATE_KEY");
+        address L2_T1_MESSENGER = vm.envAddress("L2_T1_MESSENGER_PROXY_ADDR");
+        address T1_PROXY_ADMIN_ADDR = vm.envAddress("L2_PROXY_ADMIN_ADDR");
+        ProxyAdmin proxyAdmin = ProxyAdmin(T1_PROXY_ADMIN_ADDR);
+
+        vm.startBroadcast(L2_DEPLOYER_PRIVATE_KEY);
+
+        T1XChainRead impl = new T1XChainRead(address(L2_T1_MESSENGER), origin);
+        logAddress("L2_T1_X_CHAIN_READ_IMPLEMENTATION_ADDR", address(impl));
+
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(impl),
+            address(proxyAdmin),
+            new bytes(0)
+        );
+        logAddress("L2_T1_X_CHAIN_READ_PROXY_ADDR", address(proxy));
+
+        T1XChainRead(payable(proxy)).initialize(L2_T1_MESSENGER);
+
+        vm.stopBroadcast();
+
+        logEnd("DeployXChainRead to t1");
+    }
+
+    function deploy_to_pr1() external {
+        logStart("DeployXChainRead to PR1");
+
+        uint256 L1_DEPLOYER_PRIVATE_KEY = vm.envUint("L1_DEPLOYER_PRIVATE_KEY");
+        address L1_T1_MESSENGER = vm.envAddress("L1_T1_MESSENGER_PROXY_ADDR");
+        address L1_PROXY_ADMIN_ADDR = vm.envAddress("L1_PROXY_ADMIN_ADDR");
+        ProxyAdmin proxyAdmin = ProxyAdmin(L1_PROXY_ADMIN_ADDR);
+
+        vm.startBroadcast(L1_DEPLOYER_PRIVATE_KEY);
+
+        T1XChainRead impl = new T1XChainRead(address(L1_T1_MESSENGER), pr1);
+        logAddress("L1_T1_X_CHAIN_READ_IMPLEMENTATION_ADDR", address(impl));
+
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(impl),
+            address(proxyAdmin),
+            new bytes(0)
+        );
+        logAddress("L1_T1_X_CHAIN_READ_PROXY_ADDR", address(proxy));
+
+        T1XChainRead(payable(proxy)).initialize(L1_T1_MESSENGER);
+
+        vm.stopBroadcast();
+
+        logEnd("DeployXChainRead to PR1");
+    }
+}
