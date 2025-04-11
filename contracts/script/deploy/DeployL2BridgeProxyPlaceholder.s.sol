9d355053bc7f18077120a16713b54e239576b97c
Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki+1@gmail.com>
2025-04-11 12:55:21 +0200

    fix: bump ETH granded to L2T1Messenger to 300k

--- contracts/script/deploy/DeployL2BridgeProxyPlaceholder.s.sol
+++ contracts/script/deploy/DeployL2BridgeProxyPlaceholder.s.sol
@@ -52,7 +52,7 @@ contract DeployL2BridgeProxyPlaceholder is Script, DeploymentUtils {
     }
 
     function deployL2T1Messenger() internal {
-        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy{ value: 1000 ether }(
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy{ value: 300000 ether }(
             address(placeholder), address(proxyAdmin), new bytes(0)
         );
 
