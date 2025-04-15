bb8bf143cfc8a1a1ede9a66ca4c6935fb99fa9da
evchip <evchip@pm.me>
2025-04-15 11:58:43 +0100

    test(T1Chain.t.sol): update calls to finalizeBatchWithProof

--- contracts/src/test/T1Chain.t.sol
+++ contracts/src/test/T1Chain.t.sol
@@ -1671,7 +1671,7 @@ contract T1ChainTest is Test {
         bytes memory invalidSig = new bytes(64); // 64 bytes, not 65
 
         vm.expectRevert(T1Chain.ErrorIncorrectSignatureLength.selector);
-        rollup.finalizeBatchWithProof(dummyRoot, invalidSig);
+        rollup.finalizeBatchWithProof(0, dummyRoot, invalidSig);
     }
 
     /**
@@ -1688,7 +1688,7 @@ contract T1ChainTest is Test {
         vm.expectRevert(abi.encodeWithSelector(T1Chain.ErrorIncorrectSigner.selector, expectedBadSigner));
 
         // This call should revert with `ErrorIncorrectSigner(badSigner)`
-        rollup.finalizeBatchWithProof(dummyRoot, sig);
+        rollup.finalizeBatchWithProof(0, dummyRoot, sig);
     }
 
     /**
@@ -1703,10 +1703,11 @@ contract T1ChainTest is Test {
         bytes memory sig = _signWithdrawRoot(VALID_SIGNER_KEY, withdrawRoot);
 
         // Expect it to succeed
-        rollup.finalizeBatchWithProof(withdrawRoot, sig);
+        uint256 batchIndex = 1;
+        rollup.finalizeBatchWithProof(batchIndex, withdrawRoot, sig);
 
-        assertEq(rollup.lastFinalizedBatchIndex(), 1);
-        assertEq(rollup.withdrawRoots(1), withdrawRoot);
+        assertEq(rollup.lastFinalizedBatchIndex(), batchIndex);
+        assertEq(rollup.withdrawRoots(batchIndex), withdrawRoot);
     }
 
     /**
@@ -1720,6 +1721,6 @@ contract T1ChainTest is Test {
         bytes memory sig = _signWithdrawRoot(VALID_SIGNER_KEY, dummyRoot);
 
         vm.expectRevert(bytes("Pausable: paused"));
-        rollup.finalizeBatchWithProof(dummyRoot, sig);
+        rollup.finalizeBatchWithProof(0, dummyRoot, sig);
     }
 }
