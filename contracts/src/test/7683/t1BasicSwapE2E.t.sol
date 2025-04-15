a0f539998f29e8ab2d2a248e54569684d3dfeeb7
Diego Garcia <diego.gm@pm.me>
2025-04-15 15:36:53 +0200

    chore(test): t1 -\> T1

--- contracts/src/test/7683/t1BasicSwapE2E.t.sol
+++ contracts/src/test/7683/t1BasicSwapE2E.t.sol
@@ -45,7 +45,7 @@ event Refund(bytes32[] orderIds);
 
 event Refunded(bytes32 orderId, address receiver);
 
-contract t1BasicSwapE2E is BaseTest {
+contract T1BasicSwapE2E is BaseTest {
     event Filled(bytes32 orderId, bytes originData, bytes fillerData);
 
     using TypeCasts for address;
