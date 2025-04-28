f48197912754bc40e0813f3c08043aa986a0f575
evchip <evchip@pm.me>
2025-04-28 20:57:09 +0100

    refactor(Deploy XChainReader): add signer args to constructors

--- contracts/script/deploy/DeployPR1T1XChainReader.s.sol
+++ contracts/script/deploy/DeployPR1T1XChainReader.s.sol
@@ -20,11 +20,12 @@ contract DeployPR1T1XChainReader is DeploymentUtils {
         uint256 L1_DEPLOYER_PRIVATE_KEY = vm.envUint("L1_DEPLOYER_PRIVATE_KEY");
         address L1_T1_MESSENGER = vm.envAddress("L1_T1_MESSENGER_PROXY_ADDR");
         address L1_PROXY_ADMIN_ADDR = vm.envAddress("L1_PROXY_ADMIN_ADDR");
+        address L1_SIGNER = vm.envAddress("L1_SIGNER");
         ProxyAdmin proxyAdmin = ProxyAdmin(L1_PROXY_ADMIN_ADDR);
 
         vm.startBroadcast(L1_DEPLOYER_PRIVATE_KEY);
 
-        T1XChainReader impl = new T1XChainReader(address(L1_T1_MESSENGER), PR1);
+        T1XChainReader impl = new T1XChainReader(address(L1_T1_MESSENGER), L1_SIGNER, PR1);
         logAddress("L1_T1_X_CHAIN_READ_IMPLEMENTATION_ADDR", address(impl));
 
         TransparentUpgradeableProxy proxy =
