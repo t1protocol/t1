f018baa8592c693f0369b74d40e2743bb636385b
Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki+1@gmail.com>
2025-05-16 10:55:15 +0200

    fix: selecting fork before starting broadcast

--- contracts/script/configure/Setup7683BotsLiquidity.s.sol
+++ contracts/script/configure/Setup7683BotsLiquidity.s.sol
@@ -71,8 +71,8 @@ contract Setup7683BotsLiquidity is Script, DeploymentUtils {
         WrappedEther(L1_WETH_ADDR).approve(L1_T1_PULL_BASED_7683_PROXY_ADDR, wethAmount / 2);
 
         vm.stopBroadcast();
-        vm.startBroadcast(L2_7683_BOT_PRIVATE_KEY);
         vm.createSelectFork(vm.rpcUrl("t1"));
+        vm.startBroadcast(L2_7683_BOT_PRIVATE_KEY);
 
         // approve L2 7683 Escrow to transfer USDT in the bot's name
         T1StandardERC20(L2_USDT_ADDR).approve(L2_T1_7683_PROXY_ADDR, usdtAmount / 2);
