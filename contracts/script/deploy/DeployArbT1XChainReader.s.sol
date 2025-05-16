b8a9e7899e87be874a5a3d2f13ab85abe5f03135
evchip <evchip@pm.me>
2025-05-16 18:23:09 +0100

    feat(DeployT1XChainReader): write scripts to deploy to base and arb sepolia

---
+++ contracts/script/deploy/DeployArbT1XChainReader.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.25;
+
+import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import { DeploymentUtils } from "../lib/DeploymentUtils.sol";
+
+import { T1XChainReader } from "../../src/libraries/xChain/T1XChainReader.sol";
+
+contract DeployArbT1XChainReader is DeploymentUtils {
+    function run() external {
+        vm.createSelectFork(vm.rpcUrl("arbitrum_sepolia"));
+        logStart("DeployXChainRead to ARB");
+
+        uint256 DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address ARB_T1_MESSENGER_PROXY_ADDR = vm.envAddress("ARB_T1_MESSENGER_PROXY_ADDR");
+        address ARB_T1_PROXY_ADMIN_ADDR = vm.envAddress("ARB_T1_PROXY_ADMIN_ADDR");
+        address SIGNER = vm.envAddress("ARB_SIGNER");
+        ProxyAdmin proxyAdmin = ProxyAdmin(ARB_T1_PROXY_ADMIN_ADDR);
+
+        vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
+
+        T1XChainReader impl = new T1XChainReader(address(ARB_T1_MESSENGER_PROXY_ADDR), SIGNER);
+        logAddress("ARB_T1_X_CHAIN_READ_IMPLEMENTATION_ADDR", address(impl));
+
+        TransparentUpgradeableProxy proxy =
+            new TransparentUpgradeableProxy(address(impl), address(proxyAdmin), new bytes(0));
+        logAddress("ARB_T1_X_CHAIN_READ_PROXY_ADDR", address(proxy));
+
+        vm.stopBroadcast();
+
+        logEnd("DeployXChainRead to ARB");
+    }
+}
