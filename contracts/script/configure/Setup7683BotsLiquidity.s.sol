2bbefac5b4bf6f01a50b78f89f2221d1625a53ce
Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki+1@gmail.com>
2025-05-16 10:56:54 +0200

    fix: use correct key to submit approvals

--- contracts/script/configure/Setup7683BotsLiquidity.s.sol
+++ contracts/script/configure/Setup7683BotsLiquidity.s.sol
@@ -14,6 +14,7 @@ import { Usdt } from "../deploy/DeployL1Usdt.s.sol";
 
 contract Setup7683BotsLiquidity is Script, DeploymentUtils {
     uint256 private L1_DEPLOYER_PRIVATE_KEY = vm.envUint("L1_DEPLOYER_PRIVATE_KEY");
+    uint256 private L2_DEPLOYER_PRIVATE_KEY = vm.envUint("L2_DEPLOYER_PRIVATE_KEY");
     uint256 private L1_7683_BOT_PRIVATE_KEY = vm.envUint("L1_7683_BOT_PRIVATE_KEY");
     uint256 private L2_7683_BOT_PRIVATE_KEY = vm.envUint("L2_7683_BOT_PRIVATE_KEY");
 
@@ -72,7 +73,7 @@ contract Setup7683BotsLiquidity is Script, DeploymentUtils {
 
         vm.stopBroadcast();
         vm.createSelectFork(vm.rpcUrl("t1"));
-        vm.startBroadcast(L2_7683_BOT_PRIVATE_KEY);
+        vm.startBroadcast(L2_DEPLOYER_PRIVATE_KEY);
 
         // approve L2 7683 Escrow to transfer USDT in the bot's name
         T1StandardERC20(L2_USDT_ADDR).approve(L2_T1_7683_PROXY_ADDR, usdtAmount / 2);
