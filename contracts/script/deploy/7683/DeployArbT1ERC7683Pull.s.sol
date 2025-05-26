ef567566cc63219f30164e1cb87dcd9804357b1e
Evan Chipman <42247026+evchip@users.noreply.github.com>
2025-05-26 17:18:16 +0100

    ERC-7683 deployment scripts for Arbitrum Sepolia and Base Sepolia (#111)
    Co-authored-by: Wojciech Wołoszyn <w-woloszyn@users.noreply.github.com>

---
+++ contracts/script/deploy/7683/DeployArbT1ERC7683Pull.s.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import { DeploymentUtils } from "../../lib/DeploymentUtils.sol";
+
+import { T1ERC7683Pull } from "../../../src/7683/T1ERC7683Pull.sol";
+import { T1Constants } from "../../../src/libraries/constants/T1Constants.sol";
+
+contract DeployArbT1ERC7683Pull is DeploymentUtils {
+    uint32 internal constant ARB = uint32(T1Constants.ARBITRUM_SEPOLIA_CHAIN_ID);
+    uint32 internal constant BASE = uint32(T1Constants.BASE_SEPOLIA_CHAIN_ID);
+    ProxyAdmin private proxyAdmin;
+
+    function deploy() external {
+        vm.createSelectFork(vm.rpcUrl("arbitrum_sepolia"));
+        logStart("DeployT1ERC7683Pull to Arbitrum");
+        uint256 deployerPk = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address ARB_T1_X_CHAIN_READ_PROXY_ADDR = vm.envAddress("ARB_T1_X_CHAIN_READ_PROXY_ADDR");
+        address ARB_T1_PROXY_ADMIN_ADDR = vm.envAddress("ARB_T1_PROXY_ADMIN_ADDR");
+        proxyAdmin = ProxyAdmin(ARB_T1_PROXY_ADMIN_ADDR);
+
+        vm.startBroadcast(deployerPk);
+
+        T1ERC7683Pull impl = new T1ERC7683Pull(
+            address(0), // No Permit2 for now
+            ARB_T1_X_CHAIN_READ_PROXY_ADDR,
+            ARB
+        );
+
+        TransparentUpgradeableProxy proxy =
+            new TransparentUpgradeableProxy(address(impl), address(proxyAdmin), new bytes(0));
+
+        vm.stopBroadcast();
+
+        logAddress("ARB_T1_PULL_BASED_7683_IMPLEMENTATION_ADDR", address(impl));
+        logAddress("ARB_T1_PULL_BASED_7683_PROXY_ADDR", address(proxy));
+
+        logEnd("DeployT1ERC7683Pull to Arbitrum");
+    }
+
+    function init() external {
+        vm.createSelectFork(vm.rpcUrl("arbitrum_sepolia"));
+        uint256 deployerPk = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address ARB_T1_7683_PROXY_ADDR = vm.envAddress("ARB_T1_PULL_BASED_7683_PROXY_ADDR");
+        address BASE_T1_7683_PROXY_ADDR = vm.envAddress("BASE_T1_PULL_BASED_7683_PROXY_ADDR");
+
+        vm.startBroadcast(deployerPk);
+
+        T1ERC7683Pull(ARB_T1_7683_PROXY_ADDR).initialize(BASE_T1_7683_PROXY_ADDR);
+
+        vm.stopBroadcast();
+    }
+}
---
+++ contracts/script/deploy/DeployBaseT1XChainReader.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.25;
+
+import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import { DeploymentUtils } from "../lib/DeploymentUtils.sol";
+import { T1XChainReader } from "../../src/libraries/xChain/T1XChainReader.sol";
+
+contract DeployBaseT1XChainReader is DeploymentUtils {
+    function run() external {
+        vm.createSelectFork(vm.rpcUrl("base_sepolia"));
+        logStart("DeployXChainRead to BASE");
+
+        uint256 DEPLOYER_PRIVATE_KEY = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address BASE_T1_MESSENGER_PROXY_ADDR = vm.envAddress("BASE_T1_MESSENGER_PROXY_ADDR");
+        address BASE_T1_PROXY_ADMIN_ADDR = vm.envAddress("BASE_T1_PROXY_ADMIN_ADDR");
+        address SIGNER = vm.envAddress("BASE_SIGNER");
+        ProxyAdmin proxyAdmin = ProxyAdmin(BASE_T1_PROXY_ADMIN_ADDR);
+
+        vm.startBroadcast(DEPLOYER_PRIVATE_KEY);
+
+        T1XChainReader impl = new T1XChainReader(address(BASE_T1_MESSENGER_PROXY_ADDR), SIGNER);
+        logAddress("BASE_T1_X_CHAIN_READ_IMPLEMENTATION_ADDR", address(impl));
+
+        TransparentUpgradeableProxy proxy =
+            new TransparentUpgradeableProxy(address(impl), address(proxyAdmin), new bytes(0));
+        logAddress("BASE_T1_X_CHAIN_READ_PROXY_ADDR", address(proxy));
+
+        vm.stopBroadcast();
+
+        logEnd("DeployXChainRead to BASE");
+    }
+}
