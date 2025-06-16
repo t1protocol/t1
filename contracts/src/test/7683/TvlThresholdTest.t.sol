4ba1d58a9e105e7e875f958d6fc0223b9fe614f5
Alexandre Wolff <55669233+aharvet@users.noreply.github.com>
2025-06-16 13:55:14 +0200

    test: add openFor happy path test (#127)

--- contracts/src/test/7683/TvlThresholdTest.t.sol
+++ contracts/src/test/7683/TvlThresholdTest.t.sol
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import { TypeCasts } from "@hyperlane-xyz/libs/TypeCasts.sol";
+import { ITransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import { ISignatureTransfer } from "@uniswap/permit2/src/interfaces/IPermit2.sol";
+
 import { BaseTest } from "./BaseTest.sol";
 import { T1XChainReader } from "../../libraries/xChain/T1XChainReader.sol";
 import { T1ERC7683 } from "../../7683/T1ERC7683.sol";
-
-import { TypeCasts } from "@hyperlane-xyz/libs/TypeCasts.sol";
-import { ITransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import { OrderData, OrderEncoder } from "../../../src/libraries/7683/OrderEncoder.sol";
-import { OnchainCrossChainOrder, GaslessCrossChainOrder } from "../../../src/interfaces/IERC7683.sol";
+import {
+    OnchainCrossChainOrder,
+    GaslessCrossChainOrder,
+    ResolvedCrossChainOrder
+} from "../../../src/interfaces/IERC7683.sol";
 
 contract TvlThresholdTest is BaseTest {
     using TypeCasts for address;
+    using TypeCasts for bytes32;
 
     event Paused(address account);
     event Unpaused(address account);
@@ -32,7 +38,7 @@ contract TvlThresholdTest is BaseTest {
         t1ERC7683 = T1ERC7683(payable(_deployProxy(address(0))));
         admin.upgrade(
             ITransparentUpgradeableProxy(address(t1ERC7683)),
-            address(new T1ERC7683(address(0), address(reader), uint32(origin)))
+            address(new T1ERC7683(permit2, address(reader), uint32(origin)))
         );
         t1ERC7683.initialize(address(t1ERC7683));
     }
@@ -73,19 +79,53 @@ contract TvlThresholdTest is BaseTest {
         OrderData memory orderData = _prepareOrderData();
         OnchainCrossChainOrder memory order =
             _prepareOnchainOrder(OrderEncoder.encode(orderData), orderData.fillDeadline, OrderEncoder.orderDataType());
-        bytes32 orderId = OrderEncoder.id(orderData);
 
         vm.startPrank(kakaroto);
         inputToken.approve(address(t1ERC7683), type(uint256).max);
         t1ERC7683.open(order);
         vm.stopPrank();
 
+        bytes32 orderId = OrderEncoder.id(orderData);
         bytes32 status = t1ERC7683.orderStatus(orderId);
-
         assertEq(status, t1ERC7683.OPENED());
     }
 
-    // function test_canOpenForWhenNotPaused() public { }
+    function test_canOpenForWhenNotPaused() public {
+        vm.prank(kakaroto);
+        inputToken.approve(permit2, type(uint256).max);
+
+        uint32 openDeadline = uint32(block.timestamp + 100);
+        OrderData memory orderData = _prepareOrderData();
+        GaslessCrossChainOrder memory order = _prepareGaslessOrder(
+            address(t1ERC7683),
+            kakaroto,
+            orderData.originDomain,
+            OrderEncoder.encode(orderData),
+            orderData.senderNonce,
+            openDeadline,
+            orderData.fillDeadline,
+            OrderEncoder.orderDataType()
+        );
+        bytes memory originFillerData = new bytes(0);
+
+        ResolvedCrossChainOrder memory resolvedOrder = t1ERC7683.resolveFor(order, originFillerData);
+        bytes memory sig = _getSignature(
+            address(t1ERC7683),
+            t1ERC7683.witnessHash(resolvedOrder),
+            orderData.inputToken.bytes32ToAddress(),
+            order.nonce,
+            orderData.amountIn,
+            openDeadline,
+            kakarotoPK
+        );
+
+        vm.prank(vegeta);
+        t1ERC7683.openFor(order, sig, new bytes(0));
+
+        bytes32 orderId = OrderEncoder.id(orderData);
+        bytes32 status = t1ERC7683.orderStatus(orderId);
+        assertEq(status, t1ERC7683.OPENED());
+    }
 
     function test_cannotOpenWhenPaused() public {
         t1ERC7683.pause();
