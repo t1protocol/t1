9bdedd91bc3e4baaf41cfeff0387106f765dc6d8
Krzysztof Spisak-Spisacki <kss@t1protocol.com>
2025-07-22 23:23:32 +0200

    feat: added WebSocket server to HTTP one

--- tee-apps/sealed-bid-auction/src/api/SealedBidAuctionApiServer.ts
+++ tee-apps/sealed-bid-auction/src/api/SealedBidAuctionApiServer.ts
@@ -2,6 +2,10 @@ import {SealedBidAuctionController} from "./SealedBidAuctionController.ts";
 import {WinstonLogger} from "../utils/WinstonLogger.ts";
 import type {Server} from "bun";
 
+type AuthData = {
+    token: string;
+};
+
 export class SealedBidAuctionApiServer {
 
     private logger: WinstonLogger = new WinstonLogger(SealedBidAuctionApiServer.name);
@@ -15,12 +19,36 @@ export class SealedBidAuctionApiServer {
         }
 
         const auctionController = new SealedBidAuctionController();
+        const infoLogger = this.logger.info;
 
-        this.server = Bun.serve({
+        // @ts-ignore
+        this.server = Bun.serve<AuthData>({
             port,
             routes: {
                 "/healthcheck": new Response("OK"),
                 "/api/auction": req => auctionController.auction(req),
+            },
+            fetch(req, server) {
+                const success = server.upgrade(req, {
+                    data: {
+                        token: req.headers.get("Authorization")
+                    }
+                });
+                if (success) {
+                    // Bun automatically returns a 101 Switching Protocols
+                    // if the upgrade succeeds
+                    return undefined;
+                }
+
+                return new Response("Hello WebSocket!");
+            },
+            websocket: {
+                async message(ws, message) {
+                    infoLogger(`Received ${message}`);
+                    infoLogger(`Auth token [${ws.data.token}]`);
+                    // send back a message
+                    ws.send(`You said: ${message}`);
+                },
             },
         });
 
