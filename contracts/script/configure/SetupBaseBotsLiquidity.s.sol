093ceb0f29e99d4f6a61a6f221be6935df986e9b
Ryan Collins <ry@rycollins.com>
2025-07-24 10:43:45 -0400

    chore: larger allowance for liquidity script

--- contracts/script/configure/SetupBaseBotsLiquidity.s.sol
+++ contracts/script/configure/SetupBaseBotsLiquidity.s.sol
@@ -65,9 +65,9 @@ contract Setup7683BotsLiquidity is Script, DeploymentUtils {
         // *** ERC-20 Contract Approvals *** //
         vm.startBroadcast(FILL_BOT_PRIVATE_KEY);
         // approve L1 7683 Escrow to transfer USDT in the bot's name
-        T1StandardERC20(USDT_ADDR).approve(PULL_BASED_7683_PROXY_ADDR, usdtAmount / 2);
+        T1StandardERC20(USDT_ADDR).approve(PULL_BASED_7683_PROXY_ADDR, 999_999 * 1e6);
         // approve L1 7683 Escrow to transfer WETH in the bot's name
-        WrappedEther(WETH_ADDR).approve(PULL_BASED_7683_PROXY_ADDR, wethAmount / 2);
+        WrappedEther(WETH_ADDR).approve(PULL_BASED_7683_PROXY_ADDR, 999_999 * 1e18);
 
         vm.stopBroadcast();
 
