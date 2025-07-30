97e5281ec04fea81bb69afd4f8cf7ef7ec8ac6a5
Alexandre <alexandrewolff.github@gmail.com>
2025-07-30 16:30:08 +0200

    chore: bump up solidity version

--- contracts/src/7683/T1ERC7683.sol
+++ contracts/src/7683/T1ERC7683.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity 0.8.30;
 
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
