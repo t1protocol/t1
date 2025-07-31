92410d49d12b56427ef14fa2426f68c43273ab02
Krzysztof Spisak-Spisacki <kss@t1protocol.com>
2025-07-31 14:08:38 +0200

    fix: correctly calculating amountOut

--- tee-apps/sealed-bid-auction/src/core/AuctionService.ts
+++ tee-apps/sealed-bid-auction/src/core/AuctionService.ts
@@ -5,7 +5,7 @@ import type {SolverPriceBook} from "./SolverPriceBook.ts";
 import type {Interval, PriceListItem} from "./types.ts";
 
 type Price = {
-    price: bigint;
+    amountOut: bigint;
     solverAddress: string;
 }
 
@@ -23,14 +23,14 @@ export class AuctionService {
             return {
                 id: request.id,
                 request: request as Omit<AuctionRequest, "id">,
-                amountOut: bestPrice.price,
+                amountOut: bestPrice.amountOut,
                 solverAddress: bestPrice.solverAddress,
                 timestamp: Date.now()
             };
         }
     }
 
-    private getCorrectInterval(priceListItem: PriceListItem, amount: bigint): Interval | undefined {
+    private getFinalInterval(priceListItem: PriceListItem, amount: bigint): Interval | undefined {
         return priceListItem.intervals.find(interval => interval.range.min <= amount && interval.range.max >= amount);
     }
 
@@ -38,32 +38,49 @@ export class AuctionService {
         let bestPrice = null;
 
         if (!pricesForAskedTokens.isEmpty()) {
-            let bestPrice = {
-                price: -1n,
+            let bestPrice: Price = {
+                amountOut: -1n,
                 solverAddress: "0xdeadbeef"
             };
 
             pricesForAskedTokens.forEach(price => {
-                if (price.price > bestPrice.price) bestPrice = price;
+                if (price.amountOut > bestPrice.amountOut) bestPrice = price;
             });
         }
 
         return bestPrice;
     }
 
-    private findPricesForAskedTokens(request: AuctionRequest) {
+    private findPricesForAskedTokens(request: AuctionRequest): Immutable.List<Price> {
         return this.solverPricebook.getCurrentPrices().flatMap(
             priceItems => priceItems.filter(
                 priceItem =>
                     priceItem.srcTokenAddresses.includes(request.srcTokenAddress) &&
                     priceItem.dstTokenAddress.includes(request.dstTokenAddress) &&
-                    this.getCorrectInterval(priceItem, request.amountIn) !== undefined
+                    this.getFinalInterval(priceItem, request.amountIn) !== undefined
             ).map(priceItem => {
                 return {
-                    price: this.getCorrectInterval(priceItem, request.amountIn)!.price,
+                    amountOut: this.calculateAmountOut(priceItem, request.amountIn),
                     solverAddress: priceItem.solverAddress
                 };
             })
         )
+    }
+
+    private calculateAmountOut(priceItem: PriceListItem, amountIn: bigint): bigint {
+        let currentInterval = 0;
+        let amountOut = 0n;
+        const finalInterval = this.getFinalInterval(priceItem, amountIn);
+
+        do {
+            const curr = priceItem.intervals[currentInterval]!;
+            if (curr !== finalInterval) {
+                amountOut += curr.price * (curr.range.max - curr.range.min);
+            } else {
+                amountOut += curr.price * (amountIn - curr.range.min - 1n);
+            }
+        } while (priceItem.intervals[currentInterval++] !== finalInterval);
+
+        return amountOut;
     }
 }
