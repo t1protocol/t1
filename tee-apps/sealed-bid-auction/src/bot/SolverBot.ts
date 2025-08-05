4032fe2a15049b5f46865c88fa5827bdb7da5099
Krzysztof Spisak-Spisacki <kss@t1protocol.com>
2025-08-05 03:04:23 +0200

    feat: SolverBot.ts implementation

---
+++ tee-apps/sealed-bid-auction/src/bot/SolverBot.ts
@@ -0,0 +1,51 @@
+import {USERNAME} from "../../test/constants.ts";
+import type {PriceListItem} from "../core/types.ts";
+import {serialize} from "../utils/WinstonLogger.ts";
+
+export class SolverBot {
+
+    private socket: WebSocket | null;
+
+    constructor(private readonly hostName: string = 'localhost', private readonly serverPort: number) {
+        this.socket = null;
+    }
+
+    public async start(socketResponseConsumer: (sockerResponse: string) => void) {
+        this.socket = new WebSocket(`ws://${this.hostName}:${this.serverPort}/`, {
+            headers: {
+                Authorization: USERNAME
+            }
+        });
+        this.socket.onmessage = (event) => {
+            socketResponseConsumer(event.data);
+        };
+
+        await this.waitForSocketState(WebSocket.OPEN);
+    }
+
+    public async stop() {
+        if (this.socket) {
+            this.socket.close();
+
+            await this.waitForSocketState(WebSocket.CLOSED);
+        }
+    }
+
+    public send(message: PriceListItem[]) {
+        if (!this.socket) {
+            throw Error("Socket is closed");
+        }
+
+        this.socket.send(serialize(message));
+    }
+
+    private async waitForSocketState(state: 0 | 1 | 2 | 3, timeoutMs: number = 100) {
+        if (!this.socket) {
+            throw Error("Socket is closed");
+        }
+
+        while (this.socket.readyState !== state) {
+            await new Promise((resolve) => setTimeout(resolve, timeoutMs));
+        }
+    }
+}
