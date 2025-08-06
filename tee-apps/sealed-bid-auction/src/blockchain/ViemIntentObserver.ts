47d8510528971535b93be890dd585dd068f356fb
Krzysztof Spisak-Spisacki <kss@t1protocol.com>
2025-08-06 03:48:52 +0200

    fix: wrap in array

--- tee-apps/sealed-bid-auction/src/blockchain/ViemIntentObserver.ts
+++ tee-apps/sealed-bid-auction/src/blockchain/ViemIntentObserver.ts
@@ -61,7 +61,7 @@ export class ViemIntentObserver {
         for (const order of parsedLogs) {
             for (const fillInstruction of order.args.resolvedOrder.fillInstructions) {
                 const [decodedOrder] = decodeAbiParameters(ORDER_DATA_ABI_PARAMETERS_WRAPPED_IN_TUPLE, fillInstruction.originData);
-                const orderData = convertSolidityOrderDataToTypescriptOrderData(decodedOrder);
+                const orderData = convertSolidityOrderDataToTypescriptOrderData([decodedOrder]);
 
                 auctionPromises.push(this.runAuctionAndNotifySolver(orderData, order.args.orderId));
             }
