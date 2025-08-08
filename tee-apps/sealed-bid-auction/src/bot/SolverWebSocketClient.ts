2ec6ef89fec869f3f02361dfad0c77b587cecc91
Krzysztof Spisak-Spisacki <kss@t1protocol.com>
2025-08-08 03:20:47 +0200

    feat: configuring WS URL for the bot separately

--- tee-apps/sealed-bid-auction/src/bot/SolverWebSocketClient.ts
+++ tee-apps/sealed-bid-auction/src/bot/SolverWebSocketClient.ts
@@ -6,17 +6,15 @@ export class SolverWebSocketClient {
 
     private socket: WebSocket | null;
 
-    constructor(private readonly serverPort: number, private readonly hostName: string = 'localhost') {
+    constructor(private readonly serverUrl: string) {
         this.socket = null;
     }
 
-    public async start(username: string, socketResponseConsumer: (sockerResponse: string) => void, tls: boolean) {
+    public async start(username: string, socketResponseConsumer: (sockerResponse: string) => void) {
         this.logger = new WinstonLogger(`SolverWebSocketClient[${username}]`);
 
-        const serverUrl = `ws${tls ? 's' : ''}://${this.hostName}:${this.serverPort}/`;
-
-        this.logger.info(`Connecting to server ${serverUrl}`);
-        this.socket = new WebSocket(serverUrl, {
+        this.logger.info(`Connecting to server ${this.serverUrl}`);
+        this.socket = new WebSocket(this.serverUrl, {
             headers: {
                 Authorization: username
             }
