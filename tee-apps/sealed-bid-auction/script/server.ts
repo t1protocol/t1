36564bb135f00e6c951f9b9904cef138785b4e61
Krzysztof Spisak-Spisacki <kss@t1protocol.com>
2025-08-12 01:37:40 +0200

    chore: changed unit from ms to s for more flexibility

--- tee-apps/sealed-bid-auction/script/server.ts
+++ tee-apps/sealed-bid-auction/script/server.ts
@@ -11,9 +11,9 @@ import {BlockchainClient} from "../src/blockchain/BlockchainClient.ts";
 import {ViemAuctionCommiter} from "../src/blockchain/ViemAuctionCommiter.ts";
 
 const USE_TLS = process.env.USE_TLS as string === "true";
-const SOLVER_PRICE_TTL_MS = process.env.SOLVER_PRICE_TTL_MS as string;
+const SOLVER_PRICE_TTL_SECONDS = process.env.SOLVER_PRICE_TTL_SECONDS as string;
 
-const solverPriceBook = new SolverPriceBook(SOLVER_PRICE_TTL_MS ? Number(SOLVER_PRICE_TTL_MS) : undefined);
+const solverPriceBook = new SolverPriceBook(SOLVER_PRICE_TTL_SECONDS ? Number(SOLVER_PRICE_TTL_SECONDS) : undefined);
 const auctionService = new AuctionService(solverPriceBook);
 
 const httpServer = new AuctionApiServer(solverPriceBook, auctionService);
