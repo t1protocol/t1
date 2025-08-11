b1128debda0b017ee90712373b655546afa584ce
Krzysztof Spisak-Spisacki <kss@t1protocol.com>
2025-08-12 01:30:56 +0200

    feat: made solver price TTL configurable from .env

--- tee-apps/sealed-bid-auction/script/server.ts
+++ tee-apps/sealed-bid-auction/script/server.ts
@@ -11,8 +11,9 @@ import {BlockchainClient} from "../src/blockchain/BlockchainClient.ts";
 import {ViemAuctionCommiter} from "../src/blockchain/ViemAuctionCommiter.ts";
 
 const USE_TLS = process.env.USE_TLS as string === "true";
+const SOLVER_PRICE_TTL = process.env.SOLVER_PRICE_TTL as string;
 
-const solverPriceBook = new SolverPriceBook();
+const solverPriceBook = new SolverPriceBook(SOLVER_PRICE_TTL ? Number(SOLVER_PRICE_TTL) : undefined);
 const auctionService = new AuctionService(solverPriceBook);
 
 const httpServer = new AuctionApiServer(solverPriceBook, auctionService);
