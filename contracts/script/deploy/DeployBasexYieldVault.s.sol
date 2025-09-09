75e388cd292995814c26c330dc10d0015e2dad65
Krzysztof Spisak-Spisacki <kss@t1protocol.com>
2025-09-09 11:22:33 +0200

    chore: quality of life improvements when deploying xYield (#180)

--- contracts/script/deploy/DeployBasexYieldVault.s.sol
+++ contracts/script/deploy/DeployBasexYieldVault.s.sol
@@ -8,13 +8,13 @@ import { xYieldVault } from "../../src/xYield/xYieldVault.sol";
 import { T1Constants } from "../../src/libraries/constants/T1Constants.sol";
 
 contract DeployBasexYieldVault is DeploymentUtils {
-    function deploy() external {
+    function run() external {
         vm.createSelectFork(vm.rpcUrl("base"));
         logStart("DeployxYieldVault to Base");
 
         uint256 deployerPk = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        address underlyingToken = vm.envAddress("BASE_UNDERLYING_TOKEN_ADDR");
-        address guardian = vm.envAddress("BASE_XYIELD_GUARDIAN_ADDR");
+        address underlyingToken = vm.envAddress("BASE_USDC_ADDR");
+        address guardian = vm.envAddress("XYIELD_GUARDIAN_ADDR");
         address yieldProtocol = vm.envAddress("BASE_YIELD_PROTOCOL_ADDR");
         string memory name = vm.envString("BASE_XYIELD_VAULT_NAME");
         string memory symbol = vm.envString("BASE_XYIELD_VAULT_SYMBOL");
