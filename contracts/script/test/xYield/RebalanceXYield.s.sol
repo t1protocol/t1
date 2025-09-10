5f1c665addae27d05886b724cee8e292246e4866
evchip <evchip@pm.me>
2025-09-11 03:33:00 +0700

    feat(xYield): add scripts for interacting with vaults

---
+++ contracts/script/test/xYield/RebalanceXYield.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.25;
+
+import { Script } from "forge-std/Script.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { xYieldVault } from "../../../../src/xYield/xYieldVault.sol";
+
+contract RebalanceXYield is Script {
+  uint256 guardianPk = vm.envUint("GUARDIAN_PRIVATE_KEY");
+  address underlyingToken = vm.envAddress("ARB_USDC_ADDR");
+  address xYieldVaultAddress = vm.envAddress("ARB_XYIELD_VAULT_ADDR");
+
+  function run() external {
+    vm.createSelectFork(vm.rpcUrl("arbitrum"));
+    vm.startBroadcast(guardianPk);
+    address guardian = vm.addr(guardianPk);
+    xYieldVault xYield = xYieldVault(xYieldVaultAddress);
+
+    uint256 rebalanceId = 1;
+    uint32 targetChain = 8453; // BASE CHAIN ID
+    uint256 amountIn = 2e6;
+    uint256 amountOut = 1e6;
+    uint32 acrossApiQuoteTimestamp = uint32(block.timestamp - 100);
+
+    xYield.rebalance(
+        1,
+        targetChain,
+        amountIn,
+        amountOut,
+        acrossApiQuoteTimestamp
+    );
+
+    vm.stopBroadcast();
+  }
+}
