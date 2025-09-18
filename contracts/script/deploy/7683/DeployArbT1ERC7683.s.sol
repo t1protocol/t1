3947ccdf4e849fd798d78e40e498a408786b01d2
KSS <kss@t1protocol.com>
2025-09-18 19:58:40 +0200

    fix: made sure that first part of the script executes

--- contracts/script/deploy/7683/DeployArbT1ERC7683.s.sol
+++ contracts/script/deploy/7683/DeployArbT1ERC7683.s.sol
@@ -10,11 +10,12 @@ import { T1ERC7683 } from "../../../src/7683/T1ERC7683.sol";
 import { T1Constants } from "../../../src/libraries/constants/T1Constants.sol";
 
 contract DeployArbT1ERC7683 is DeploymentUtils {
-    address internal ARB_T1_X_CHAIN_READ_PROXY_ADDR = vm.envAddress("ARB_T1_X_CHAIN_READ_PROXY_ADDR");
     address internal ARB_T1_PROXY_ADMIN_ADDR = vm.envAddress("ARB_T1_PROXY_ADMIN_ADDR");
     address internal ARB_T1_7683_PROXY_ADDR = vm.envAddress("ARB_T1_PULL_BASED_7683_PROXY_ADDR");
-    address internal BASE_T1_7683_PROXY_ADDR = vm.envAddress("BASE_T1_PULL_BASED_7683_PROXY_ADDR");
     address internal AUCTION_WITNESS = vm.envAddress("AUCTION_WITNESS");
+
+    address internal ARB_T1_X_CHAIN_READ_PROXY_ADDR = vm.envOr("ARB_T1_X_CHAIN_READ_PROXY_ADDR", address(0));
+    address internal BASE_T1_7683_PROXY_ADDR = vm.envOr("BASE_T1_PULL_BASED_7683_PROXY_ADDR", address(0));
 
     uint32 internal immutable ARB =
         uint32(IS_MAINNET ? T1Constants.ARBITRUM_MAINNET_CHAIN_ID : T1Constants.ARBITRUM_SEPOLIA_CHAIN_ID);
