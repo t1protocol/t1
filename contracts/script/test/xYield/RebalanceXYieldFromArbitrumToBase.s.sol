2eceb0d1400d2005d5d310f605d247cde67a37df
evchip <evchip@pm.me>
2025-09-22 15:58:40 +0700

    refactor(RebalanceXYieldFromArbitrumToBase): rename + use pseudorandom rebalance ID

--- contracts/script/test/xYield/RebalanceXYieldFromArbitrumToBase.s.sol
+++ contracts/script/test/xYield/RebalanceXYieldFromArbitrumToBase.s.sol
@@ -7,7 +7,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { xYieldVault } from "../../../../src/xYield/xYieldVault.sol";
 
-contract RebalanceXYield is Script {
+contract RebalanceXYieldFromArbitrumToBase is Script {
     uint256 guardianPk = vm.envUint("GUARDIAN_PRIVATE_KEY");
     address underlyingToken = vm.envAddress("ARB_USDC_ADDR");
     address xYieldVaultAddress = vm.envAddress("ARB_XYIELD_VAULT_ADDR");
@@ -18,13 +18,13 @@ contract RebalanceXYield is Script {
         address guardian = vm.addr(guardianPk);
         xYieldVault xYield = xYieldVault(xYieldVaultAddress);
 
-        uint256 rebalanceId = 1;
+        uint256 rebalanceId = uint256(keccak256(abi.encodePacked(block.timestamp, block.chainid)));
         uint32 targetChain = 8453; // BASE CHAIN ID
         uint256 amountIn = 2e6;
         uint256 amountOut = 1e6;
         uint32 acrossApiQuoteTimestamp = uint32(block.timestamp - 100);
 
-        xYield.rebalance(1, targetChain, amountIn, amountOut, acrossApiQuoteTimestamp);
+        xYield.rebalance(rebalanceId, targetChain, amountIn, amountOut, acrossApiQuoteTimestamp);
 
         vm.stopBroadcast();
     }
