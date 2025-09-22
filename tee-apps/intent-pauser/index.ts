e241c19d2c527ed8158f91efffc188d7c9f115bf
KSS <kss@t1protocol.com>
2025-09-23 00:48:50 +0200

    feat: read eth price from coingecko

--- tee-apps/intent-pauser/index.ts
+++ tee-apps/intent-pauser/index.ts
@@ -14,12 +14,19 @@ interface NetworkConfig {
     usdc: `0x${string}`;
 }
 
-async function startMonitor(config: NetworkConfig, privateKey: `0x${string}`, thresholdUsd: number, unpauseThresholdUsd: number, pollIntervalMs: number) {
+async function startMonitor(
+    config: NetworkConfig,
+    pauserPrivateKey: `0x${string}`,
+    thresholdUsd: number,
+    unpauseThresholdUsd: number,
+    pollIntervalMs: number,
+    ethpriceFallback : number
+) {
     const client = new T1ERC7683Client(
         config.rpcUrl,
         config.contractAddress,
         contractAbi.abi as any,
-        privateKey,
+        pauserPrivateKey,
         config.chain
     );
 
@@ -28,7 +35,7 @@ async function startMonitor(config: NetworkConfig, privateKey: `0x${string}`, th
         {token: config.usdc, decimals: 6, symbol: "USDC"},
     ];
 
-    const monitor = new ThresholdMonitor(client, monitoredTokens, thresholdUsd, unpauseThresholdUsd);
+    const monitor = new ThresholdMonitor(client, monitoredTokens, thresholdUsd, unpauseThresholdUsd, ethpriceFallback);
 
     console.log(`[${config.chain.name}] Starting ThresholdMonitor with threshold=${thresholdUsd} USD, interval=${pollIntervalMs}ms`);
 
@@ -51,6 +58,7 @@ async function main() {
     const pauseThresholdUsd = Number(process.env.PAUSE_USD_THRESHOLD);
     const unpauseThresholdUsd = Number(process.env.UNPAUSE_USD_THRESHOLD);
     const pollIntervalMs = Number(process.env.POLL_INTERVAL_MS);
+    const ethpriceFallback = Number(process.env.ETH_PRICE_FALLBACK);
 
     const networks: NetworkConfig[] = [
         {
@@ -70,7 +78,7 @@ async function main() {
     ];
 
     networks.forEach((network) => {
-        startMonitor(network, pauserPrivateKey, pauseThresholdUsd, unpauseThresholdUsd, pollIntervalMs);
+        startMonitor(network, pauserPrivateKey, pauseThresholdUsd, unpauseThresholdUsd, pollIntervalMs, ethpriceFallback);
     });
 }
 
--- tee-apps/intent-pauser/src/TresholdMonitor.ts
+++ tee-apps/intent-pauser/src/TresholdMonitor.ts
@@ -1,12 +1,12 @@
 import { T1ERC7683Client } from "./T1ERC7683Client";
 
 export class ThresholdMonitor {
-
     constructor(
         private readonly client: T1ERC7683Client,
         private readonly monitoredErc20Tokens: { token: `0x${string}`; decimals: number; symbol: string }[],
         private readonly pauseUsdThreshold: number,
         private readonly unpauseUsdThreshold: number,
+        private readonly ethPriceFallback: number
     ) { }
 
     public async pauseOrUnpauseIfNeeded(): Promise<`0x${string}` | null> {
@@ -35,16 +35,38 @@ export class ThresholdMonitor {
                 const rawBal = await this.client.getErc20Balance(token);
                 const normalized = rawBal / 10n ** BigInt(decimals);
                 const price = await this.getUsdPrice(symbol);
-                return normalized * price;
+                return normalized * BigInt(price);
             })
         );
 
         return balances.reduce((a, b) => a + b, 0n);
     }
 
-    private async getUsdPrice(symbol: string): Promise<bigint> {
-        if (symbol === "WETH") return 4100n;
-        if (symbol === "USDC") return 1n;
-        return 0n;
+    private async getUsdPrice(symbol: string): Promise<number> {
+        let result = 0;
+
+        if (symbol === "USDC") result = 1;
+
+        if (symbol === "WETH") {
+            const coingeckoPrice = await this.readEthPriceFromCoingecko();
+
+            coingeckoPrice ? result = coingeckoPrice : result = this.ethPriceFallback;
+        }
+
+        return result;
+    }
+
+    private async readEthPriceFromCoingecko(): Promise<number | null> {
+        try {
+            const res = await fetch(
+                "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+            );
+            const data = await res.json() as { symbol: string, price: string };
+
+            return Number(data.price);
+        } catch (e) {
+            console.error("Error fetching ETH price from Coingecko:", e);
+            return null;
+        }
     }
 }
