9dcb6618cd2477c8356692cea1c4003d8cbd650e
Evan Chipman <42247026+evchip@users.noreply.github.com>
2025-09-22 19:33:05 +0700

    feat(xYield): add scripts for interacting with vaults (#182)

---
+++ contracts/script/deploy/xYield/SetBaseSiblingVaultOnArbxYieldVault.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { DeploymentUtils } from "../../lib/DeploymentUtils.sol";
+import { xYieldVault } from "../../../src/xYield/xYieldVault.sol";
+
+contract SetBaseSiblingVaultOnArbxYieldVault is DeploymentUtils {
+    function run() external {
+        vm.createSelectFork(vm.rpcUrl("arbitrum"));
+        logStart("set Base sibling vault on Arbitrum xYieldVault");
+
+        uint256 deployerPk = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address usdcBase = vm.envAddress("BASE_USDC_ADDR");
+        address xYieldArbitrumAddr = vm.envAddress("ARB_XYIELD_VAULT_ADDR");
+        address xYieldBase = vm.envAddress("BASE_XYIELD_VAULT_ADDR");
+        address multicallHandlerBase = vm.envAddress("BASE_ACROSS_MULTICALL_HANDLER_ADDR");
+        uint64 baseChainId = 8453;
+
+        vm.startBroadcast(deployerPk);
+
+        xYieldVault xYieldArbitrum = xYieldVault(xYieldArbitrumAddr);
+
+        xYieldArbitrum.setSiblingVault(baseChainId, address(xYieldBase), address(usdcBase), multicallHandlerBase);
+
+        vm.stopBroadcast();
+
+        logEnd("set Base sibling vault on Arbitrum xYieldVault");
+    }
+}
