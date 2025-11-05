f9841a1d16f5ba52311da727f5e07de441e45e29
KSS <kss@t1protocol.com>
2025-11-05 12:54:08 +0100

    chore: precise comment

--- contracts/src/xYield/MintableERC20.sol
+++ contracts/src/xYield/MintableERC20.sol
@@ -59,8 +59,7 @@ contract MintableERC20 is ERC20 {
     /// @dev The standard burn function is already public and allows any address to burn their own tokens,
     /// emitting a Transfer event to address(0) as the burn signal. Thus, no custom burn event is defined.
 
-    /// @notice Burns tokens
-    /// @param account The address to burn tokens from
+    /// @notice Burns own tokens
     /// @param amount The amount of tokens to burn
     function burn(uint256 amount) external {
         _burn(_msgSender(), amount);
