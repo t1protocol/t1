8c2c4322518c9a9ca9e341fda36b3ba0b2704591
Ryan Collins <rymcol@users.noreply.github.com>
2025-12-17 19:41:22 -0500

    Feature: proxy admin owner on deploy (#220)

--- contracts/script/deploy/DeployArbT1ProxyAdmin.s.sol
+++ contracts/script/deploy/DeployArbT1ProxyAdmin.s.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.25;
 import { Script } from "forge-std/Script.sol";
 import { ProxyAdmin } from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import { TransparentUpgradeableProxy } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 import { DeploymentUtils } from "../lib/DeploymentUtils.sol";
 
@@ -18,10 +19,21 @@ contract DeployArbT1ProxyAdmin is Script, DeploymentUtils {
         startBroadcastWithDeployerKeyIfItExists();
 
         deployProxyAdmin();
+        transferProxyAdminOwnershipIfNeeded();
+
+        vm.stopBroadcast();
     }
 
     function deployProxyAdmin() internal {
         proxyAdmin = new ProxyAdmin();
         logAddress("ARB_T1_PROXY_ADMIN_ADDR", address(proxyAdmin));
+    }
+
+    function transferProxyAdminOwnershipIfNeeded() internal {
+        address proxyAdminOwner = vm.envOr("ARB_T1_PROXY_ADMIN_OWNER_ADDR", address(0));
+        if (proxyAdminOwner != address(0)) {
+            Ownable(address(proxyAdmin)).transferOwnership(proxyAdminOwner);
+            logAddress("ARB_T1_PROXY_ADMIN_OWNER_ADDR", proxyAdminOwner);
+        }
     }
 }
