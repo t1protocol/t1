788055f9789c5b4017f9ef5f057c2291fdc589c0
KSS <kss@t1protocol.com>
2025-12-31 12:54:00 +0100

    feat: don't process last block again as it's very unlikely WS drops mid-block processing

--- tee-apps/sealed-bid-auction/src/blockchain/ViemIntentObserver.ts
+++ tee-apps/sealed-bid-auction/src/blockchain/ViemIntentObserver.ts
@@ -53,7 +53,7 @@ export class ViemIntentObserver {
       event: OPEN_INTENT_ABI_EVENT,
       onLogs: async (logs) => {
         if (this.websocketError && this.lastProcessedBlock) {
-          await this.fetchHistoricalLogs(this.lastProcessedBlock + 1);
+          await this.fetchHistoricalLogs(this.lastProcessedBlock + 1n);
           this.websocketError = false;
         }
         await this.processIntentLogs(logs);
