301c8bd002690bdfb0391e897d3886e19bf0d5fa
KSS <kss@t1protocol.com>
2026-01-09 14:27:58 +0100

    chore: renames for clarity

--- contracts/script/test/7683/analyselogs/arb-to-base.ts
+++ contracts/script/test/7683/analyselogs/arb-to-base.ts
@@ -1,29 +1,29 @@
 import { ethers } from "ethers";
 import { analyzeERC7683Logs } from "./src/logAnalyzer7683.ts";
 
-const sourceChainProvider = new ethers.JsonRpcProvider("https://arb1.arbitrum.io/rpc");
-const sourceChainErc7683Address = "0x996f3583bd967bba19694733aa7a7623e6d780eb";
+const arbitrumProvider = new ethers.JsonRpcProvider("https://arb1.arbitrum.io/rpc");
+const arbitrumErc7683Address = "0x996f3583bd967bba19694733aa7a7623e6d780eb";
 
-const destinationChainProvider = new ethers.JsonRpcProvider("https://base-rpc.publicnode.com");
-const destinationChainErc7683Address = "0xdbA711a6c1b187479e9a5b33020E5217D0BD5A1f";
+const baseProvider = new ethers.JsonRpcProvider("https://base-rpc.publicnode.com");
+const baseErc7683Address = "0xdbA711a6c1b187479e9a5b33020E5217D0BD5A1f";
 
-const sourceChainFromBlock = 418241010;
-const sourceChainLatest = 418842847;
+const arbitrumFromBlock = 417132015;
+const arbitrumToBlock = 418842847;
 // const sourceChainLatest = await sourceChainProvider.getBlockNumber();
 
-const destinationChainFromBlock = 40423900;
-const destinationChainLatest = 40499026;
+const baseFromBlock = 40423900;
+const baseToBlock = 40499026;
 // const destinationChainLatest    = await destinationChainProvider.getBlockNumber();
 
 await analyzeERC7683Logs({
   sourceChainName: "Arbitrum",
-  sourceChainFromBlock,
-  sourceChainLatest,
-  sourceChainProvider,
-  sourceChainErc7683Address,
+  sourceChainFromBlock: arbitrumFromBlock,
+  sourceChainLatest: arbitrumToBlock,
+  sourceChainProvider: arbitrumProvider,
+  sourceChainErc7683Address: arbitrumErc7683Address,
   destinationChainName: "Base",
-  destinationChainFromBlock,
-  destinationChainLatest,
-  destinationChainProvider,
-  destinationChainErc7683Address,
+  destinationChainFromBlock: baseFromBlock,
+  destinationChainLatest: baseToBlock,
+  destinationChainProvider: baseProvider,
+  destinationChainErc7683Address: baseErc7683Address,
 })
--- contracts/script/test/7683/analyselogs/base-to-arb.ts
+++ contracts/script/test/7683/analyselogs/base-to-arb.ts
@@ -1,29 +1,29 @@
 import { ethers } from "ethers";
 import { analyzeERC7683Logs } from "./src/logAnalyzer7683.ts";
 
-const sourceChainProvider = new ethers.JsonRpcProvider("https://base-rpc.publicnode.com");
-const sourceChainErc7683Address = "0xdbA711a6c1b187479e9a5b33020E5217D0BD5A1f";
+const baseProvider = new ethers.JsonRpcProvider("https://base-rpc.publicnode.com");
+const baseErc7683Address = "0xdbA711a6c1b187479e9a5b33020E5217D0BD5A1f";
 
 const destinationChainProvider = new ethers.JsonRpcProvider("https://arb1.arbitrum.io/rpc");
 const destinationChainErc7683Address = "0x996f3583bd967bba19694733aa7a7623e6d780eb";
 
-const sourceChainFromBlock = 40423900;
-const sourceChainLatest = 40499026;
-// const sourceChainLatest    = await sourceChainProvider.getBlockNumber();
+const baseFromBlock = 40423900;
+const baseToBlock = 40499026;
+// const baseToBlock    = await baseProvider.getBlockNumber();
 
-const destinationChainFromBlock = 418241010;
-const destinationChainLatest = 418842847;
-// const destinationChainLatest = await destinationChainProvider.getBlockNumber();
+const arbitrumFromBlock = 418241010;
+const arbitrumToBlock = 418842847;
+// const arbitrumToBlock = await destinationChainProvider.getBlockNumber();
 
 await analyzeERC7683Logs({
   sourceChainName: "Base",
-  sourceChainFromBlock,
-  sourceChainLatest,
-  sourceChainProvider,
-  sourceChainErc7683Address,
+  sourceChainFromBlock: baseFromBlock,
+  sourceChainLatest: baseToBlock,
+  sourceChainProvider: baseProvider,
+  sourceChainErc7683Address: baseErc7683Address,
   destinationChainName: "Arbitrum",
-  destinationChainFromBlock,
-  destinationChainLatest,
+  destinationChainFromBlock: arbitrumFromBlock,
+  destinationChainLatest: arbitrumToBlock,
   destinationChainProvider,
   destinationChainErc7683Address,
 })
